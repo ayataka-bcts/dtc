a1989ebec2911e9d7e4f36e34b3518736e2e9f96
ayataka_bcts <scene.of.bloodshed232@gmail.com>
2024-05-14 22:24:20 +0900

    [update]ディレクトリ構成を調整

--- Assets/Scripts/Audio/BGMPath.cs
+++ Assets/Scripts/Audio/BGMPath.cs
@@ -8,6 +8,7 @@ public static class BGMPath{
 	public const string BATTLE27    = "BGM/Battle/battle27";
 	public const string FANTASY14   = "BGM/fantasy14";
 	public const string HEARTBEAT01 = "BGM/heartbeat01";
+	public const string JAILBREAK   = "BGM/Jailbreak";
 
 }
 
